24417b8bd8c8852c673e9ccfadb8d03602c29fd9
Mofeleng <mofelengmokgope@gmail.com>
2025-03-07 22:03:12 +0200

    Updated schemas and added a new favicon

--- src/lib/constants/schemas.ts
+++ src/lib/constants/schemas.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const formSchema = z.object({
-  name: z.string().min(2, 'Name must at least be 2 charecter'),
-  number: z.string().min(10, 'Please enter a valid phone number'),
-  email: z.string().email('Please enter a valid email')
+  name: z.string().min(2, {message: 'Name must at least be 2 charecter'}),
+  number: z.string().min(10, {message: 'Please enter a valid phone number'}),
+  email: z.string().email({message: 'Please enter a valid email'})
 });
